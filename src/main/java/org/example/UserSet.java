952e6d9ceebacd79ee057ab544289dcb21adead5
hector918 <hector_zhong@outlook.com>
2025-04-14 15:31:20 -0400

    finished

--- src/main/java/org/example/UserSet.java
+++ src/main/java/org/example/UserSet.java
@@ -1,7 +1,69 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-public class UserSet {}
+public class UserSet {
+
+    private final HashSet<String> users = new HashSet<>();
+
+    public boolean containsUser(String username){
+        return users.contains(username);
+    }
+    public boolean removeUser(String username){
+        try{
+            if(users.contains(username)){
+                users.remove(username);
+                return true;
+            }
+            return false;
+
+        }catch(Exception e){
+            System.out.println(e.toString());
+            return false;
+        }
+    }
+    public int getUserCount(){
+        return users.size();
+    }
+    public List<String> getUsersWithPrefix(String prefix){
+        if (prefix == null) {
+            throw new IllegalArgumentException("prefix cannot be null");
+        }
+        List<String> ret = new ArrayList<>();
+        for(String user : users){
+            if(user.startsWith(prefix)){
+                ret.add(user);
+            }
+        }
+        return ret;
+    }
+    public boolean addUser(String username){
+        if(username == null){
+            throw new IllegalArgumentException("username cannot be null");
+        }
+
+        if(username.trim().isEmpty()){
+            throw new IllegalArgumentException("username cannot be empty");
+        }
+
+        try {
+            if(!users.contains(username)){
+                users.add(username);
+                return true;
+            }
+            return false;
+
+        }catch (Exception e){
+            System.out.println(e.toString());
+            return false;
+        }
+    }
+
+    public boolean areAllUsersUnique(String[] arr){
+        if (arr == null) {
+            throw new IllegalArgumentException("arr cannot be null");
+        }
+        Set<String> nameSet = new HashSet<>(Arrays.asList(arr));
+        return arr.length == nameSet.size();
+    }
+}
